f2bdcf60849fb55b0c0e42dafcb77d6f788f09a8
NazarovIlya <eliakohai@gmail.com>
2022-09-22 18:22:10 +0300

    minus N to N

---
+++ TasksPy/Task003_WriteNToN.py
@@ -0,0 +1,13 @@
+# Вывести на экран числа от -N до N.
+
+numberN = int(input(f'Введите целое число: '))
+if numberN > 0:
+    minN = numberN * -1
+else:
+    minN =  numberN
+    numberN *= -1
+if numberN > minN:
+    while minN < numberN:
+        print(f'{minN} ')
+        minN += 1
+else: print(0)
