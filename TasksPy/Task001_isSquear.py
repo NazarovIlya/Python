2fac0b7b55e79d7fabd998da1a51fcea9eb59a52
NazarovIlya <eliakohai@gmail.com>
2022-09-23 15:34:29 +0300

    updated

--- TasksPy/Task001_isSquear.py
+++ TasksPy/Task001_isSquear.py
@@ -1,10 +1,13 @@
 # По двум заданным числам проверить является ли одно квадратом второго.
 
-firstNumber = int(input('Введите целое число: '))
-secondNumber = int(input('Введите целое число: '))
-if firstNumber / secondNumber == secondNumber:
-    print(f'Число {firstNumber}(первое) являетя квадратом числа {secondNumber}(второго)')
-elif secondNumber / firstNumber == firstNumber:
-    print(f'Число {secondNumber}(второе) являетя квадратом числа {firstNumber}(первого)')
-else:
-    print(f'Ни одно число не является квадратом другого')
+def isSquare(firstNumber, secondNumber):
+    if firstNumber == secondNumber ** 2:
+        print(f'Число {firstNumber}(первое) являетя квадратом числа {secondNumber}(второго)')
+    elif secondNumber == firstNumber ** 2:
+        print(f'Число {secondNumber}(второе) являетя квадратом числа {firstNumber}(первого)')
+    else:
+        print(f'Ни одно число не является квадратом другого')
+
+first_number = int(input('Введите целое число: '))
+second_number = int(input('Введите целое число: '))
+isSquare(first_number, second_number)
