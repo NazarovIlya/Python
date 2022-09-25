6098d520668d258f3d4bfae178121241b7174c0c
NazarovIlya <eliakohai@gmail.com>
2022-09-25 15:31:17 +0300

    updated

--- TasksPy/Task003_WriteNToN.py
+++ TasksPy/Task003_WriteNToN.py
@@ -1,15 +1,16 @@
 # Вывести на экран числа от -N до N.
 
 numberN = int(input(f'Введите целое число: '))
-# if numberN > 0:                           // My solution
-#     minN = numberN * -1
-# else:
-#     minN =  numberN
-#     numberN *= -1
-# if numberN > minN:
-#     while minN < numberN:
-#         print(f'{minN} ', end = '')
-#         minN += 1
-# else: print(0)
-for i in range(-numberN, numberN + 1):      # alternative solution
-    print(f'{i} ', end = '')
+if numberN > 0:                              # My solution
+    minN = numberN * -1
+else:
+    minN =  numberN
+    numberN *= -1
+if numberN > minN:
+    while minN <= numberN:
+        print(f'{minN} ', end = '')
+        minN += 1
+else: print(0)
+
+# for i in range(-numberN, numberN + 1):      # alternative solution
+#     print(f'{i} ', end = '')
