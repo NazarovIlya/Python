0dbe05ebcd277a533c8e327a5e0393fc015e0ec4
NazarovIlya <eliakohai@gmail.com>
2022-09-30 21:48:54 +0300

    product_pair_numbers

--- TasksPy/Task023_product_pair_numbers.py
+++ TasksPy/Task023_product_pair_numbers.py
@@ -9,3 +9,21 @@
 def input_list():
     user_list = input('Введите несколько чисел через пробел: ').split(' ')
     return user_list
+
+def find_product(user_list):
+    result_list = []
+    product = 1
+    length = int(len(user_list ) / 2)
+    for i in range(length):
+        product = int(user_list[i]) * int(user_list[len(user_list) - 1 - i])
+        result_list.append(product)
+        if length % 2 != 0:
+            product = int(user_list[length]) ** 2
+            result_list.append(product)
+    return result_list
+
+
+user_lst = input_list()
+print(user_lst)
+result = find_product(user_lst)
+print(result)
