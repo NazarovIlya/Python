74774d0de88bec26a551818350aeb0e3134d7239
NazarovIlya <eliakohai@gmail.com>
2022-10-03 19:39:30 +0300

    get minimum & maximum in the string

--- TasksPy/Task027_get_min_max_in_string.py
+++ TasksPy/Task027_get_min_max_in_string.py
@@ -1,2 +1,24 @@
 # Задайте строку из набора чисел. Напишите программу, которая покажет большее и
 # меньшее число. В качестве символа-разделителя используйте пробел.
+
+
+def input_list():
+    user_list = input('Введите несколько вещественных чисел через пробел: ').split(' ')
+    return user_list
+
+
+def get_min_max(user_string):
+    min_value = user_string[0]
+    max_value = user_string[0]
+    for current_value in user_string:
+        if int(current_value) < int(min_value):
+            min_value = current_value
+        elif int(current_value) > int(max_value):
+            max_value = current_value
+    return min_value, max_value
+
+
+numbers_list = input_list()
+print(*numbers_list)
+result = get_min_max(numbers_list)
+print(f'Минимальное найденное значение = {result[0]}, максимальное найденное значение = {result[1]}')
