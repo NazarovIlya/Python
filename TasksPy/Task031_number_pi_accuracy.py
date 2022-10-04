83452e6191b897d8163b1874b3416cedf0cd0f8e
NazarovIlya <eliakohai@gmail.com>
2022-10-04 21:08:31 +0300

    updated

--- TasksPy/Task031_number_pi_accuracy.py
+++ TasksPy/Task031_number_pi_accuracy.py
@@ -8,11 +8,15 @@ import math
 
 def get_count_sings(number):
     signs = 0
-    while number < 1:
-        number *= 10
-        signs += 1
-    pi = math.pi
-    return round(pi, signs)
+    if 0.1 >= number >= 0.0000000001:
+        while number < 1:
+            number *= 10
+            signs += 1
+        pi = math.pi
+        return round(pi, signs)
+    else:
+        print('Введите корретные данные в диапазоне от 10^-1 до 10*^-10: ')
+        quit()
         
 
 num = float(input('Введите точность числа, которую хотели бы получить форматом, например 0.001: '))        
