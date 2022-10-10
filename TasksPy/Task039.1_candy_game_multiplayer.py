095460c613f91334ec45302de5461d05e9dbe612
NazarovIlya <eliakohai@gmail.com>
2022-10-10 22:19:35 +0300

    multiplayer

--- TasksPy/Task039.1_candy_game_multiplayer.py
+++ TasksPy/Task039.1_candy_game_multiplayer.py
@@ -13,3 +13,56 @@
 #! Мультиплеер
 
 
+import random
+
+
+def greeting(player_count):
+    names_list = []
+    for i in range(player_count):
+        names_list.append(input(f'Введите имя игрока №{i + 1}: '))
+        print(f'Привет, {names_list[i]}')
+    return names_list
+
+
+def player_motion(names, count, item):
+    if count < 28:
+        last_candy = count
+    else:
+        last_candy = 28
+    if count > 0:
+        motion = int(input(f'{names[item]}, Ваш ход. Выберите от 1 до {last_candy} конфет: '))
+        if motion < 1 and motion > 28:
+            count -= motion
+        else:
+            print(f'{motion} брать конфет(ы) нельзя, переход хода.')
+        if count == 0:
+            print('Поздравляем! Вы забрали оставшиеся конфеты и выиграли их все.')
+            quit()
+    return count
+
+
+def candy_game(names, count):
+    last_candy = 28
+    draw = random.randint(0, len(names) - 1)
+    motion = int(input(f'{names[draw]} начинает игру. Сделайте ход, возмите от 1 до {last_candy}: '))
+    if motion < 1 and motion > 28:
+        count -= motion
+    else:
+        print(f'{motion} брать конфет(ы) нельзя, переход хода.')
+    i = 0
+    count = player_motion(names, count, i)
+    while True:
+        i = 0
+        count = player_motion(names, count, i)
+        i += 1
+        count = player_motion(names, count, i)
+        if count == 0:
+            break
+           
+
+candy_count = 60 #! 2021
+player_count = 2
+player_names = greeting(player_count)
+candy_game(player_names, candy_count)
+
+
