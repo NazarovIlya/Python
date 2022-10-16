f0e8cd28004d33e8f0d899e02b5e84a6fafebd1b
NazarovIlya <eliakohai@gmail.com>
2022-10-16 15:19:55 +0300

    random values

--- ExamplesPy/Ex001_System/data_provider.py
+++ ExamplesPy/Ex001_System/data_provider.py
@@ -5,9 +5,9 @@ def get_temperature(sensor):
     return randint(-20, 0) if sensor else randint(0, 20)
 
 
-def get_pressure():
+def get_pressure(sensor):
     return randint(720, 740) if sensor else randint(740, 760)
 
 
-def get_wind_speed():
+def get_wind_speed(sensor):
     return randint(0, 15) if sensor else randint(15, 50)
