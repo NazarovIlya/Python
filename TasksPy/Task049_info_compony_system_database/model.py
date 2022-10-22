c72c842e97b2f58f0c774fd33b240dfb1b368b95
NazarovIlya <eliakohai@gmail.com>
2022-10-23 02:07:28 +0300

    select, deleted methods added

--- TasksPy/Task049_info_compony_system_database/model.py
+++ TasksPy/Task049_info_compony_system_database/model.py
@@ -2,8 +2,22 @@ import data_base_init as db
 
 
 def delete_row_by_id(name_id):
+    db.curs.execute(f'SELECT FROM personal WHERE ID={name_id}')
+    db.d_base.commit()
+
+
+def delete_row_by_somename_str(name, value):
+    db.curs.execute(f'SELECT FROM personal WHERE LIKE {name}={value}')
+    db.d_base.commit()
+
+
+def delete_row_by_id(name_id):
     db.curs.execute(f'DELETE FROM personal WHERE ID={name_id}')
     db.d_base.commit()
 
+
+def delete_row_by_somename_str(name, value):
+    db.curs.execute(f'DELETE FROM personal WHERE LIKE {name}={value}')
+    db.d_base.commit()
 
 
