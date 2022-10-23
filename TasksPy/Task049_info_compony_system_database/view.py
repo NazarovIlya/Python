9886e60f68b0295ee1efc5e96b459c3122f9c9f2
NazarovIlya <eliakohai@gmail.com>
2022-10-23 19:49:24 +0300

    UI realized

--- TasksPy/Task049_info_compony_system_database/view.py
+++ TasksPy/Task049_info_compony_system_database/view.py
@@ -11,6 +11,23 @@ def input_person_info(titles):
     return tuple(name_list)
 
 
+def input_delete_name(name_list, name_list_en):
+    name = ''
+    value = ''
+    while True:
+        name, value = input(f'Введите название({name_list[0]}, {name_list[1]}, {name_list[2]}) и значение колонки для строки, которую(ые) хотите удалить: \t').split()
+        if name.isdigit() or value.isdigit():
+            print('Ошибка ввода, корректное строковое значение.')
+        else:
+            if name == name_list[0]:
+                name = name_list_en[0]
+            elif name == name_list[1]:
+                name = name_list_en[1]
+            elif name == name_list[2]:
+                name = name_list_en[2]
+        return name, value
+
+
 def input_delete_id():
     number_id = 0
     while True:
@@ -26,4 +43,11 @@ def output_table_to_console():
     for i in db.curs.execute('SELECT * FROM  personal'):
         print(*i)
         
+
+def is_deleted(is_true):
+    if is_true == True:
+        print('Строка(ки) успешно удалена!')
+    else:
+        print('Удаление завершилось неудачей.')
+
 
