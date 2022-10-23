7f4efc6db5670e71767cd6d9431f1e40f7a8193c
NazarovIlya <eliakohai@gmail.com>
2022-10-23 19:47:36 +0300

    logic changed, not completed

--- TasksPy/Task049_info_compony_system_database/controller.py
+++ TasksPy/Task049_info_compony_system_database/controller.py
@@ -1,6 +1,3 @@
-import turtle
-
-from numpy import number
 import data_base_init as db
 import model as m
 import view as v
@@ -8,19 +5,29 @@ import view as v
 
 def run_application():
     personal_titles = db.title_list
-    #personal_info = [("Иван", "Иванов", "Генеральным директор", 250000, 80000)]
-    personal_info = []
-    person = v.input_person_info(personal_titles)
-    personal_info.append(person)
+    personal_info = [("Иван", "Иванов", "Генеральным директор", 250000, 80000)]
+    # personal_info = []
+    # person = v.input_person_info(personal_titles)
+    # personal_info.append(person)
+    
     #personal_info = v.input_person_info(personal_titles)
     
-    db.curs.executemany("INSERT INTO personal VALUES(null, ?, ?, ?, ?, ?)", personal_info)
-    db.d_base.commit()
+    # db.curs.executemany("INSERT INTO personal VALUES(null, ?, ?, ?, ?, ?)", personal_info)
+    # db.d_base.commit()
     
     
     v.output_table_to_console()
-    number = v.input_delete_id()
-    m.delete_row_by_id(number)
+    
+    # number = v.input_delete_id()
+    # is_true = m.delete_row_by_id(number)
+    # v.is_deleted(is_true)
+
+    
+    name_list = db.title_list
+    name_list_en = db.title_list_en
+    name, value = v.input_delete_name(name_list, name_list_en)
+    is_true = m.delete_row_by_somename_str(name, value)
+    v.is_deleted(is_true)
 
     # for i in db.curs.execute('SELECT * FROM  personal'):
     #     print(i)
