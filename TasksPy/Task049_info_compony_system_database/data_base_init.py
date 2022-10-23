c6d0f393e9802f1b94ecd554041ab7ad66de5393
NazarovIlya <eliakohai@gmail.com>
2022-10-23 19:46:35 +0300

    lists added, column name replaced

--- TasksPy/Task049_info_compony_system_database/data_base_init.py
+++ TasksPy/Task049_info_compony_system_database/data_base_init.py
@@ -6,10 +6,11 @@ d_base = sqlite3.connect("personal.db")
 curs = d_base.cursor()
 
 title_list = ['имя', 'фамилия', 'должность', 'заработная плата', 'премия']
+title_list_en = ['first_name', 'family', 'position,' 'salary', 'bonus']
 
 curs.execute('''CREATE TABLE IF NOT EXISTS personal(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first name TEXT,
+    first_name TEXT,
     family TEXT,
     position TEXT,
     salary INT,
