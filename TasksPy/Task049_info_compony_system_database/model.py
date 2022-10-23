ad120877db18f54f736428691b6f38d567e9618f
NazarovIlya <eliakohai@gmail.com>
2022-10-23 19:48:38 +0300

    basic commands added

--- TasksPy/Task049_info_compony_system_database/model.py
+++ TasksPy/Task049_info_compony_system_database/model.py
@@ -1,23 +1,39 @@
 import data_base_init as db
 
 
+def select_row_by_id(name_id):
+    try:
+        db.curs.execute(f'SELECT FROM personal WHERE id={name_id}')
+        db.d_base.commit()
+        return True
+    except:
+        return False
+
+
+def select_row_by_somename_str(name, value):
+    try:
+        db.curs.execute(f'SELECT FROM personal {name} WHERE LIKE "{value}"')
+        db.d_base.commit()
+        return True
+    except:
+        return False
+
+
 def delete_row_by_id(name_id):
-    db.curs.execute(f'SELECT FROM personal WHERE ID={name_id}')
-    db.d_base.commit()
+    try:
+        db.curs.execute(f'DELETE FROM personal WHERE ID={name_id}')
+        db.d_base.commit()
+        return True
+    except:
+        return False
 
 
 def delete_row_by_somename_str(name, value):
-    db.curs.execute(f'SELECT FROM personal WHERE LIKE {name}={value}')
-    db.d_base.commit()
-
-
-def delete_row_by_id(name_id):
-    db.curs.execute(f'DELETE FROM personal WHERE ID={name_id}')
-    db.d_base.commit()
-
-
-def delete_row_by_somename_str(name, value):
-    db.curs.execute(f'DELETE FROM personal WHERE LIKE {name}={value}')
-    db.d_base.commit()
+    try:
+        db.curs.execute(f'DELETE FROM personal WHERE {name} LIKE "{value}";')
+        db.d_base.commit()
+        return True
+    except:
+        return False
 
 
