2a37890d8259c9143ad6c7bc6ff209b016807ae1
NazarovIlya <eliakohai@gmail.com>
2022-10-30 05:17:29 +0300

    updated

--- TasksPy/Task051_TestFunctions/task_functions.py
+++ TasksPy/Task051_TestFunctions/task_functions.py
@@ -1,7 +1,9 @@
+# умножение ради теста ностроек рабочего окружения
 def mult(a, b):
     return a * b
 
 
+# По двум заданным числам проверить является ли одно квадратом второго.
 def is_square(firstNumber, secondNumber):
     if firstNumber == secondNumber ** 2:
         return (f'Число {firstNumber}(первое) являетя квадратом числа {secondNumber}(второго)')
@@ -10,7 +12,8 @@ def is_square(firstNumber, secondNumber):
     else:
         return (f'Ни одно число не является квадратом другого')
     
-    
+
+# Найти максимальное из пяти чисел.   
 def get_max(inputList):
     maxValue = inputList[0]
     for j in range(len(inputList)):
@@ -19,6 +22,7 @@ def get_max(inputList):
     return maxValue
 
 
+# Вывести на экран числа от -N до N.
 def sequence_minusN_to_N(numberN):
     lst = []
     if numberN > 0:                              # My solution
@@ -33,4 +37,11 @@ def sequence_minusN_to_N(numberN):
     if numberN == 0:
         lst.append(0)
     return lst
+
+
+# Показать первую цифру дробной части числа.
+def get_first_float_digit(number):
+    round_num = int(number)
+    result = (number - round_num)*10
+    return abs(int(result))
     
