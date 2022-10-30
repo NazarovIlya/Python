09d9b7b3145ab89b7189ed16c770d6acf75f6515
NazarovIlya <eliakohai@gmail.com>
2022-10-30 04:30:01 +0300

    get max

--- TasksPy/Task051_TestFunctions/task_functions.py
+++ TasksPy/Task051_TestFunctions/task_functions.py
@@ -1,6 +1,7 @@
 def mult(a, b):
     return a * b
 
+
 def is_square(firstNumber, secondNumber):
     if firstNumber == secondNumber ** 2:
         return (f'Число {firstNumber}(первое) являетя квадратом числа {secondNumber}(второго)')
@@ -8,3 +9,11 @@ def is_square(firstNumber, secondNumber):
         return (f'Число {secondNumber}(второе) являетя квадратом числа {firstNumber}(первого)')
     else:
         return (f'Ни одно число не является квадратом другого')
+    
+    
+def get_max(inputList):
+    maxValue = inputList[0]
+    for j in range(len(inputList)):
+        if inputList[j] > maxValue:
+            maxValue = inputList[j]
+    return maxValue
