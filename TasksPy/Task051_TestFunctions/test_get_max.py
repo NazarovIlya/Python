bcfa1007116e0537ce0a2f739e4353caf2bec5b3
NazarovIlya <eliakohai@gmail.com>
2022-10-30 04:27:57 +0300

    test get max

---
+++ TasksPy/Task051_TestFunctions/test_get_max.py
@@ -0,0 +1,12 @@
+import pytest
+from task_functions import get_max
+
+
+@pytest.mark.parametrize('input_list, excepted', [([3, 5, -9, 23, 2], 23),
+                                                  ([64, -345, 3145, 55, -543, 3, 6452, -315], 6452),
+                                                  ([555, 56, -13, 13, -134, 14, 5, 123], 555),
+                                                  ([342,65, -67, 3, 243, 53, -453, 341, -77], 342)])
+
+
+def test_get_max_good(input_list, excepted):
+    assert get_max(input_list) == excepted
