9ac28e7dd0c060178c1abf84df4843b6989b4cdb
NazarovIlya <eliakohai@gmail.com>
2022-10-30 04:58:11 +0300

    sequence from -N to N

---
+++ TasksPy/Task051_TestFunctions/test_sequence_minusN_to_N.py
@@ -0,0 +1,13 @@
+import pytest
+from task_functions import sequence_minusN_to_N
+
+
+@pytest.mark.parametrize('number, excepted', [(1, [-1, 0, 1]),
+                                              (3, [-3, -2, -1, 0, 1, 2, 3]),
+                                              (5, [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]),
+                                              (-5, [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]),
+                                              (0, [0])])
+
+
+def test_sequence(number, excepted):
+    assert sequence_minusN_to_N(number) == excepted
