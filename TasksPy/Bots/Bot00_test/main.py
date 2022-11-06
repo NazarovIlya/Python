51f2b5eaa5d8820dc61ff7f1a472260915b2c058
NazarovIlya <eliakohai@gmail.com>
2022-11-06 15:51:20 +0300

    updated, import corrected

--- TasksPy/Bots/Bot00_test/main.py
+++ TasksPy/Bots/Bot00_test/main.py
@@ -1,21 +1,22 @@
 import bot_config as bt
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-from bot_commands import *
+import bot_commands as bc
+# import 
 
 TOKEN = bt.TOKEN
 upd = Updater(TOKEN)
 
 
-upd.dispatcher.add_handler(CommandHandler("Start", start_command))
-upd.dispatcher.add_handler(CommandHandler("hi", hi_command))
+upd.dispatcher.add_handler(CommandHandler("Start", bc.start_command))
+upd.dispatcher.add_handler(CommandHandler("hi", bc.hi_command))
 
-upd.dispatcher.add_handler(CommandHandler("sum", sum_command))
-upd.dispatcher.add_handler(CommandHandler("sub", sub_command))
-upd.dispatcher.add_handler(CommandHandler("mult", mult_command))
-upd.dispatcher.add_handler(CommandHandler("div", div_command))
+upd.dispatcher.add_handler(CommandHandler("sum", bc.sum_command))
+upd.dispatcher.add_handler(CommandHandler("sub", bc.sub_command))
+upd.dispatcher.add_handler(CommandHandler("mult", bc.mult_command))
+upd.dispatcher.add_handler(CommandHandler("div", bc.div_command))
 
-upd.dispatcher.add_handler(CommandHandler("help", help_command))
+upd.dispatcher.add_handler(CommandHandler("help", bc.help_command))
 
 
 upd.start_polling()
