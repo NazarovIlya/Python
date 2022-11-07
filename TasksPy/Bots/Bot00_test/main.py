90811a99635e946447760ce4e0a2a7c689624284
NazarovIlya <eliakohai@gmail.com>
2022-11-08 01:07:13 +0300

    updated

--- TasksPy/Bots/Bot00_test/main.py
+++ TasksPy/Bots/Bot00_test/main.py
@@ -4,22 +4,26 @@ import bot_config as bt
 import bot_commands as bc
 import simple_calc as sc
 
-TOKEN = bt.TOKEN
-upd = Updater(TOKEN)
 
+def main():
+    TOKEN = bt.TOKEN
+    upd = Updater(TOKEN)
 
-upd.dispatcher.add_handler(CommandHandler("Start", bc.start_command))
-upd.dispatcher.add_handler(CommandHandler("hi", bc.hi_command))
+    upd.dispatcher.add_handler(CommandHandler("Start", bc.start_command))
+    upd.dispatcher.add_handler(CommandHandler("hi", bc.hi_command))
 
-upd.dispatcher.add_handler(CommandHandler("sum", sc.sum_command))
-upd.dispatcher.add_handler(CommandHandler("sub", sc.sub_command))
-upd.dispatcher.add_handler(CommandHandler("mult", sc.mult_command))
-upd.dispatcher.add_handler(CommandHandler("div", sc.div_command))
+    upd.dispatcher.add_handler(CommandHandler("sum", sc.sum_command))
+    upd.dispatcher.add_handler(CommandHandler("sub", sc.sub_command))
+    upd.dispatcher.add_handler(CommandHandler("mult", sc.mult_command))
+    upd.dispatcher.add_handler(CommandHandler("div", sc.div_command))
 
-upd.dispatcher.add_handler(CommandHandler("help", bc.help_command))
-upd.dispatcher.add_handler(TypeHandler(Update, bc.input_error))
+    upd.dispatcher.add_handler(CommandHandler("help", bc.help_command))
+    upd.dispatcher.add_handler(TypeHandler(Update, bc.input_error))
 
+    upd.start_polling()
+    print('Server started.')
+    upd.idle()
+    
 
-upd.start_polling()
-print('Server started.')
-upd.idle()
+if __name__ == "__main__":
+    main()
