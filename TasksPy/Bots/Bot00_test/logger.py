3b8314f6192716dd16aec0f739ac428cfe76df5a
NazarovIlya <eliakohai@gmail.com>
2022-11-07 20:28:27 +0300

    updated

--- TasksPy/Bots/Bot00_test/logger.py
+++ TasksPy/Bots/Bot00_test/logger.py
@@ -6,13 +6,10 @@ from datetime import datetime
 def log(update: Update, context: CallbackContext):
     with open('db.csv', 'a+', encoding= 'utf-8') as f:
         f.write('')
-    with open('db.csv', 'r', encoding= 'utf-8') as f:
+    with open('db.csv', 'r+', encoding= 'utf-8') as f:
         data = f.read().splitlines()
-        update.message.reply_text(str(data))
         if data == []:
-            with open('db.csv', 'w', encoding= 'utf-8') as f:
-                f.write('DATE; TIME; ID; FULL_NAME; USER_INPUT;\n')
-    with open('db.csv', 'a', encoding= 'utf-8') as f:
+            f.write('DATE; TIME; ID; FULL_NAME; USER_INPUT;\n')
         f.write(f'date: {datetime.now().strftime("%d-%M-%y")};'\
             f'time: {datetime.now().strftime("%H:%M")};'\
             f'id: {update.effective_user.id};'\
