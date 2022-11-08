6aa9eca3da798a9cf9c9afdb82539e103bd0e866
NazarovIlya <eliakohai@gmail.com>
2022-11-08 19:39:15 +0300

    cacher & logger decorators completed

---
+++ TasksPy/Task053_sequency_(1+n)sqr_n.py/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from datetime import datetime
+
+
+def cacher(func):
+    cache = {}
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = args
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        print(cache)
+        return cache[key]
+    
+    return wrapper
+
+
+def logger(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        args = args[0]
+        with open('log.csv', 'a+', encoding= 'utf-8') as f:
+            f.write('')
+        with open('log.csv', 'r+', encoding= 'utf-8') as f:
+            data = f.read().splitlines()
+            if data == []:
+                f.write('DATE; TIME; ARGS; RESULT;\n')
+            f.write(f'{datetime.now().strftime("%d-%M-%y")}; '\
+                    f'{datetime.now().strftime("%H:%M")}; '\
+                    f'{args}; '\
+                    f'{result}\n')
+        return result
+    return wrapper
